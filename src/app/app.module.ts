bfa5938ce3403a76b4822adbf32a5e3f8ef0690c
chrissiarivera <riverajalane@gmail.com>
2017-08-24 16:37:44 +0800

    Revert "Initial app"
    This reverts commit 771b0f07931504b3ef5dd386e9369031358b5150.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { EntryComponent } from './entry/entry.component';
-import { AddingComponent } from './entry/adding/adding.component';
-import { PrintingComponent } from './adding/printing/printing.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    EntryComponent,
-    AddingComponent,
-    PrintingComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
